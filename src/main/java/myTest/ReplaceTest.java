6832f5bff511d9bc2e5da73c571a5fd4c331c4f1
xuefeng.cai <xuefeng.cai@ipiaoniu.com>
2018-07-10 09:19:58 +0800

    ReplaceTest

---
+++ src/main/java/myTest/ReplaceTest.java
@@ -0,0 +1,12 @@
+package myTest;
+
+/**
+ * @author Stephen Cai
+ * @date 2018-07-09 11:04
+ */
+public class ReplaceTest {
+    public static void main(String[] args) {
+        String phone = " 18638179977";
+        System.out.println(phone.replaceAll("\\s",""));
+    }
+}
