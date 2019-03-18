f39fd327598439ecbeaba6252cc4b3aae14327ad
xuefeng.cai <xuefeng.cai@ipiaoniu.com>
2019-03-18 09:50:25 +0800

    leetcode 121

---
+++ src/main/java/leetcode/TreeNode.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * @author Stephen Cai
+ * @date 2019-03-17 21:50
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
--- src/main/java/leetcode/子集_78.java
+++ src/main/java/leetcode/子集_78.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,23 +10,29 @@ import java.util.List;
  *
  * 示例:
  *
- * 输入: nums = [1,2,3]
- * 输出:
- * [
- *   [3],
- *   [1],
- *   [2],
- *   [1,2,3],
- *   [1,3],
- *   [2,3],
- *   [1,2],
- *   []
- * ]
+ * 输入: nums = [1,2,3] 输出: [ [3], [1], [2], [1,2,3], [1,3], [2,3], [1,2], [] ]
+ *
  * @author Stephen Cai
  * @date 2019-03-17 20:07
  */
 public class 子集_78 {
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> tmp = new ArrayList<>();
+        dfs(res, tmp, nums, 0);
+        return res;
+    }
 
+    private void dfs(List<List<Integer>> res, List<Integer> tmp, int[] nums, int j) {
+        res.add(new ArrayList<>(tmp));
+        for (int i = j; i < nums.length; i++) {
+            tmp.add(nums[i]);
+            dfs(res, tmp, nums, i + 1);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+       new 子集_78().subsets(new int[]{1,2});
     }
 }
