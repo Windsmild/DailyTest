9ab492da90f0c8bf27d1e114a4d7d301ede6d288
xuefeng.cai <xuefeng.cai@ipiaoniu.com>
2019-03-19 11:06:52 +0800

    IP to Long

--- src/main/java/other/AminoIPConvertTest.java
+++ src/main/java/other/AminoIPConvertTest.java
@@ -15,84 +15,84 @@ public class AminoIPConvertTest {
     @Test
     public void testNull() {
         Long value = convert.parseIP4Address(null);
-        Assert.assertTrue(Objects.equal(-1l, value));
+        Assert.assertTrue(Objects.equal(-1L, value));
     }
 
     @Test
     public void testEmptyString() {
         Long value = convert.parseIP4Address("");
-        Assert.assertTrue(Objects.equal(-1l, value));
+        Assert.assertTrue(Objects.equal(-1L, value));
     }
 
     @Test
     public void testEmptyString2() {
         Long value = convert.parseIP4Address(" 172.168.5.1");
-        Assert.assertTrue(Objects.equal(-1l, value));
+        Assert.assertTrue(Objects.equal(-1L, value));
     }
 
     @Test
     public void testEmptyString3() {
         Long value = convert.parseIP4Address("172.168.5.1 ");
-        Assert.assertTrue(Objects.equal(-1l, value));
+        Assert.assertTrue(Objects.equal(-1L, value));
     }
 
     @Test
     public void testRegular() {
         Long value = convert.parseIP4Address("172.168.5.1");
-        Assert.assertTrue(Objects.equal(2896692481l, value));
+        Assert.assertTrue(Objects.equal(2896692481L, value));
     }
 
     @Test
     public void testWithSpace_01() {
         Long value = convert.parseIP4Address("172 .168.5.1");
-        Assert.assertTrue(Objects.equal(2896692481l, value));
+        Assert.assertTrue(Objects.equal(2896692481L, value));
     }
 
     @Test
     public void testWithSpace_02() {
         Long value = convert.parseIP4Address("172. 168.5.1");
-        Assert.assertTrue(Objects.equal(2896692481l, value));
+        Assert.assertTrue(Objects.equal(2896692481L, value));
     }
 
     @Test
     public void testWithSpace_03() {
         Long value = convert.parseIP4Address("172 . 168.5.1");
-        Assert.assertTrue(Objects.equal(2896692481l, value));
+        Assert.assertTrue(Objects.equal(2896692481L, value));
     }
 
     @Test
     public void testWithSpace_04() {
         Long value = convert.parseIP4Address("1 72.168.5.1");
-        Assert.assertTrue(Objects.equal(-1l, value));
+        Assert.assertTrue(Objects.equal(-1L, value));
     }
 
     @Test
     public void testWithDot_01() {
         Long value = convert.parseIP4Address("172.168.5.1.");
-        Assert.assertTrue(Objects.equal(-1l, value));
+        Assert.assertTrue(Objects.equal(-1L, value));
     }
 
     @Test
     public void testWithDot_02() {
         Long value = convert.parseIP4Address("172.168.51.");
-        Assert.assertTrue(Objects.equal(-1l, value));
+        Assert.assertTrue(Objects.equal(-1L, value));
     }
 
     @Test
     public void testWithDot_03() {
         Long value = convert.parseIP4Address("172.168.51");
-        Assert.assertTrue(Objects.equal(-1l, value));
+        Assert.assertTrue(Objects.equal(-1L, value));
     }
 
     @Test
     public void testWithDot_04() {
         Long value = convert.parseIP4Address(".172.168.51");
-        Assert.assertTrue(Objects.equal(-1l, value));
+        Assert.assertTrue(Objects.equal(-1L, value));
     }
 
     @Test
     public void testWithDot_05() {
         Long value = convert.parseIP4Address("..168.51");
-        Assert.assertTrue(Objects.equal(-1l, value));
+        Assert.assertTrue(Objects.equal(-1L, value));
     }
 }
